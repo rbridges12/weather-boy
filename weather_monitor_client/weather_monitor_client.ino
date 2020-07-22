320f44ce0796c64942025b3df00ff2c6e9fbbfa1
Riley Bridges <rlybrdgs@gmail.com>
2020-07-22 13:00:05 -0500

    troubleshooting NodeMCU

--- weather_monitor_client/weather_monitor_client.ino
+++ weather_monitor_client/weather_monitor_client.ino
@@ -4,16 +4,17 @@
 #include <ESP8266HTTPClient.h>
 
 #define DHT_PIN 16 // D0 on NodeMCU
+#define DHT_TYPE DHT11
 
-DHT weather_sensor(DHT_PIN, DHT11);
+DHT weather_sensor = DHT(DHT_PIN, DHT11);
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   weather_sensor.begin();
 }
 
 void loop() {
-  Serial.println(weather_sensor.read(DHT_PIN));
+  //Serial.println(weather_sensor.read(DHT_PIN));
   
   float humidity = weather_sensor.readHumidity();
   Serial.print("\nhumidity (%): ");
@@ -23,5 +24,5 @@ void loop() {
   Serial.print("temperature (C): ");
   Serial.println(temp);
 
-  delay(2000);
+  delay(5000);
 }
