c4b22302d3df62878cc1c2dd541535245f4f7bb2
Riley Bridges <rlybrdgs@gmail.com>
2020-07-22 12:21:05 -0500

    switched to correct DHT sensor library

--- weather_monitor_client/weather_monitor_client.ino
+++ weather_monitor_client/weather_monitor_client.ino
@@ -1,21 +1,26 @@
-#include <dht11.h>
+#include <DHT.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-#define DHT11_PIN 14 // D5 on NodeMCU
+#define DHT_PIN 16 // D0 on NodeMCU
 
-dht11 weather_sensor;
+DHT weather_sensor(DHT_PIN, DHT11);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  weather_sensor.begin();
 }
 
 void loop() {
-  Serial.println(weather_sensor.read(DHT11_PIN);
-  Serial.print("\nhumidity %: ");
-  Serial.println((float)weather_sensor.humidity, 2);
+  Serial.println(weather_sensor.read(DHT_PIN));
+  
+  float humidity = weather_sensor.readHumidity();
+  Serial.print("\nhumidity (%): ");
+  Serial.println(humidity);
+
+  float temp = weather_sensor.readTemperature();
   Serial.print("temperature (C): ");
-  Serial.println((float)weather_sensor.temperature, 2);
+  Serial.println(temp);
 
   delay(2000);
 }
