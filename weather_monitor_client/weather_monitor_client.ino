50bc27ad936398140d917537d6029614af8e836d
Riley Bridges <rlybrdgs@gmail.com>
2020-07-28 15:23:13 -0500

    copied and fixed raymond's code for the weather server

--- weather_monitor_client/weather_monitor_client.ino
+++ weather_monitor_client/weather_monitor_client.ino
@@ -4,18 +4,24 @@
 #include <ESP8266HTTPClient.h>
 
 #define DHT_PIN 16 // D0 on NodeMCU
-#define DHT_TYPE DHT11
 
-DHT weather_sensor = DHT(DHT_PIN, DHT11);
+const int port 8080;
+const char *ssid = ""; // WiFi SSID
+const char *password = ""; // WiFi password
+const char *web_host = "http://iot-testing.herokuapp.com";
+
+// DHT weather_sensor = DHT(DHT_PIN, DHT11);
+DHT weather_sensor(DHT_PIN, DHT11);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   weather_sensor.begin();
+  WiFi.begin(ssid, password);
 }
 
 void loop() {
   //Serial.println(weather_sensor.read(DHT_PIN));
-  
+
   float humidity = weather_sensor.readHumidity();
   Serial.print("\nhumidity (%): ");
   Serial.println(humidity);
