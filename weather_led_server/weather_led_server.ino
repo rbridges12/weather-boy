9171f738036201972999f067e0b81b56046c7d7f
Riley Bridges <rlybrdgs@gmail.com>
2020-07-28 17:05:31 -0500

    removed unused functions

--- weather_led_server/weather_led_server.ino
+++ weather_led_server/weather_led_server.ino
@@ -53,8 +53,6 @@ void setup()
 
     //assign handlers
     server.on("/", handle_index);
-    // server.on("/get_temp", handle_get_temp);
-    // server.on("/get_humidity", handle_get_humidity);
     server.on("/get_weather_data", handle_get_weather_data);
     server.on("/update_brightness", handle_update_brightness);
     server.on("/toggle_led", handle_toggle_led);
@@ -216,45 +214,6 @@ void handle_toggle_led()
     server.send(200, "LED has been turned on");
 }
 
-// // handler for GET request to send temp sensor value, sends as JSON
-// void handle_get_temp()
-// {
-//     // read and print  sensor values
-//     // TODO: move F/C conversion to the control server
-//     float tempC = weather_sensor.readTemperature();
-//     float tempF = weather_sensor.convertCtoF(tempC);
-//     Serial.print("Temperature (C) = ");
-//     Serial.println(tempC);
-//     Serial.print("Temperature (F) = ");
-//     Serial.println(tempF);
-
-//     // put temp data in JSON
-//     StaticJsonDocument<json_capacity> temperature_info;
-//     temperature_info["tempC"] = tempC;
-//     temperature_info["tempF"] = tempF;
-//     String serialized_temp = "";
-//     serializeJson(temperature_info, serialized_temp);
-
-//     server.send(200, "text/plain", serialized_temp);
-// }
-
-// // handler for GET request to send humidity sensor value, sends as JSON
-// void handle_get_humidity()
-// {
-//     // read and print sensor value
-//     float humidity = weather_sensor.readHumidity();
-//     Serial.print("Humidity = ");
-//     Serial.println(humidity);
-
-//     // put humidity data in JSON
-//     StaticJsonDocument<json_capacity> humidity_info;
-//     humidity_info["humidity"] = humidity;
-//     String serialized_humidity = "";
-//     serializeJson(humidity_info, serialized_humidity);
-
-//     server.send(200, "text/plain", serialized_humidity);
-// }
-
 // handler for GET request to send temp and humidity data in a JSON string
 void handle_get_weather_data()
 {
@@ -339,32 +298,5 @@ void send_ip()
     }
     client.end();
     Serial.print("HTTP code from IP send request: ");
-    Serial.println(http_code);
-}
-
-// send weather data to control server
-void store_data()
-{
-    // read sensor values
-    float tempC = weather_sensor.readTemperature();
-    float tempF = weather_sensor.convertCtoF(tempC);
-    float humidity = weather_sensor.readHumidity();
-    Serial.println(tempC);
-    Serial.println(tempF);
-    Serial.println(humidity);
-    Serial.println();
-
-    // send data to server
-    HTTPClient client;
-    String url = server_domain_name + (String) "/api/temp_control/store_data/" + (String)tempF + "/" + (String)tempC + "/" + (String)humidity;
-    client.begin(url);
-    int http_code = client.GET();
-    if (!client.connected())
-    {
-        Serial.print("could not connect to ");
-        Serial.println(url);
-    }
-    client.end();
-    Serial.print("HTTP code from weather data request: ");
     Serial.println(http_code);
 }
